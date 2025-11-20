29fa5e94934846f6210e54a96f87b173645fec9c
ritoru-cmd <kzrblazmoker121@gmail.com>
2025-11-20 07:43:37 +0700

    title

--- layouts/header.php
+++ layouts/header.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="./assets/img/apple-icon.png">
     <link rel="apple-touch-icon" sizes="76x76" href="asset/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="asset/img/favicon.png">
+    <link rel="icon" type="image/png" href="asset/img/apple-icon.png">
     <title>
-        Material Dashboard 2 by Creative Tim
+        EPERPUS
     </title>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css"
