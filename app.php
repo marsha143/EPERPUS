d6450e8e55ef874a1e35e41774c1e8c0de22344f
marsha143 <marshanda406@gmail.com>
2026-01-22 11:31:31 +0700

    perubahan

--- app.php
+++ app.php
@@ -10,25 +10,28 @@ if ($page === 'peminjaman' && $view === 'export_exel') {
 }
 ?>
 <?php
-require './config/db.php';
+include './config/db.php';
 
 if (!isset($_SESSION['login']) || $_SESSION['user']['role'] !== 'admin') {
     header('Location: login');
     exit;
 }
 
-$pages = [
-    'dashboard' => './admin/features/dashboard/index.php',
-    'anggota'   => './admin/features/anggota/index.php',
-];
-
-$page = $_GET['page'] ?? 'dashboard';
-
-if (!isset($pages[$page])) {
-    include './config/404.php';
-    exit;
-}
 
 include './admin/layouts/header.php';
 include './admin/layouts/navbar.php';
-include $pages[$page];
+
+$page = $_GET['page'] ?? 'home';
+$view = $_GET['view'] ?? 'index';
+
+
+$page = basename($page);
+$view = basename($view);
+
+$features = "./admin/features/$page/$view.php";
+
+if (file_exists($features)) {
+    include $features;
+} else {
+    include './config/404.php';
+}
--- app_anggota.php
+++ app_anggota.php
@@ -1,23 +1,24 @@
 <?php
-require './config/db.php';
+include './config/db.php';
 
 if (!isset($_SESSION['login']) || $_SESSION['user']['role'] !== 'anggota') {
     header('Location: login');
     exit;
 }
 
-$pages = [
-    'home'   => './anggota/features_anggota/home_anggota/index.php',
-    'profil' => './anggota/features_anggota/profil/index.php',
-];
-
-$page = $_GET['page'] ?? 'home';
-
-if (!isset($pages[$page])) {
-    include './config/404.php';
-    exit;
-}
-
 include './anggota/layouts_anggota/header.php';
 include './anggota/layouts_anggota/navbar.php';
-include $pages[$page];
+
+$page = $_GET['page'] ?? 'home_anggota';
+$view = $_GET['view'] ?? 'index';
+
+$page = basename($page);
+$view = basename($view);
+
+$features_anggota = "./anggota/features_anggota/$page/$view.php";
+
+if (file_exists($features_anggota)) {
+    include $features_anggota;
+} else {
+    include './config/404.php';
+}
